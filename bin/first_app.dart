a4776311496db04403ab0ae8501afeb525a9ee60
Mh_Shamey <mh.shamey@gmail.com>
2022-11-07 19:01:03 +0300

    variable and data type

---
+++ bin/first_app.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  // Variable
+ int x=50;
+ double y=49.999999;
+ String name="mohammad";
+ int age =15;
+ String s="50";
+ bool bl= 5==10 || 6>9;
+ //print(!bl);
+
+ bool and = 4>4 && 5!=4 || 7>6;
+ print(and);
+}
